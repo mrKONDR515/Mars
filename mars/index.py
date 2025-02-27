e3eb2989d2128f76eb739c7a23bded02a0feaeb7
mrKONDR515 <132822740+mrKONDR515@users.noreply.github.com>
2025-02-27 19:34:20 +0400

    Add files via upload

--- mars/index.py
+++ mars/index.py
@@ -11,5 +11,10 @@ def index():
     return render_template('index.html', title='Заготовка')
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
