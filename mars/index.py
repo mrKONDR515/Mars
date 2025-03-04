31c657b58d982162d10cffacc96f1efe02700311
mrKONDR515 <132822740+mrKONDR515@users.noreply.github.com>
2025-03-04 15:56:15 +0400

    Add files via upload

--- mars/index.py
+++ mars/index.py
@@ -47,5 +47,11 @@ def login():
     return render_template('login.html', title='Аварийный доступ', form=form)
 
 
+@app.route('/distribution')
+def distribution():
+    astronauts = ["Ридли Скотт", "Энди Уир", "Марк Уотни", "Венката Капур", "Тедди Сандерс", "Шон Бин"]
+    return render_template('distribution.html', title='Размещение по каютам', astronauts=astronauts)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
